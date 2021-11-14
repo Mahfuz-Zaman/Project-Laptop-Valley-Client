fd7cc3fe1f9353b7e0f06f46a3476b7f502ac274
Mahfuz-Zaman <zaman.13121996@.com>
2021-11-15 01:06:31 +0600

    env file update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,12 +2,12 @@
 
 console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyBEtSensC1RckQAMGJH_RoSKr8f7h_np98",
-  authDomain: "laptop-valley-assignment-12.firebaseapp.com",
-  projectId: "laptop-valley-assignment-12",
-  storageBucket: "laptop-valley-assignment-12.appspot.com",
-  messagingSenderId: "234003024817",
-  appId: "1:234003024817:web:0c2b2dd5dd6de540a34b90"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
